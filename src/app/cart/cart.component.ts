661f8dc5f8f004ec06229f8841013db27e75333d
bochen <cbrady3@sina.com>
2019-11-17 02:03:13 -0700

    AngularForm
    Add a form-based checkout feature.  Empty the cart and reset the form on form submission.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+//Forms
+import { FormBuilder } from '@angular/forms'
+
 //to use cart service
 import { CartService } from '../cart.service';
-
 
 @Component({
   selector: 'app-cart',
@@ -13,12 +15,28 @@ export class CartComponent implements OnInit {
 
   //def items property
   items: any[];
+  //def checkoutForm
+  checkoutForm: any;
   
   // inject CartService
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ){
     this.items = this.cartService.getItems(); // why within constructor ?
+    //set up checkout Form
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: '', 
+    });
+  }
+
+  onSubmit(customerData){
+    //Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+    
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 
   ngOnInit() {
